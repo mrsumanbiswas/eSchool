feadd4c6f9963f2bc9ded8985bc586cf6d5eefad
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-09-27 19:03:59 +0530

    updates

--- includes/Person.hpp
+++ includes/Person.hpp
@@ -1,18 +1,14 @@
-#ifdef person_HPP
-#define person_HPP
+#ifdef PERSON_HPP
+#define PERSON_HPP
 
 #include <string>
-
-/**
-* @brief Represents a `person` object
-*/
 
 /**
  * @brief Represents basic personal information for a person.
  */
 struct PersonalInfo {
     std::string name;            ///< The full name of the person.
-    std::string dateOfBirth;     ///< The date of birth of the person (e.g., "YYYY-MM-DD").
+    std::string dateOfBirth;     ///< The date of birth of the person (e.g., "DD-MM-YYYY").
     std::string gender;          ///< The gender of the person (e.g., Male, Female, Non-binary).
     int age;                     ///< The age of the person.
     std::vector<std::string> languagesSpoken; ///< List of languages spoken by the person.
@@ -46,15 +42,9 @@ struct Interests {
     std::vector<std::string> interests; ///< List of interests.
 };
 
-
-
-
-
-
-
-
-
-
+/**
+* @brief Represents a `person` object
+*/
 class person{
 public:
   /**
@@ -63,67 +53,13 @@ public:
   Person(); // Default constructor
 
   /**
-  * @brief Parameterized constructor for the Person class.
-  * @param name The name of the person.
-  * @param age The age of the person.
-  * @param ID The unique person ID.
-  */
-  Person(const std::string* name, int age, int ID);
-
-  /**
    * @brief Destructor for the Person class.
    */
   ~Person();
 
-  /**
-   * @brief Get the name of the person.
-   * @return The name of the person as a string.
-   */
-  std::string getName() const;
-
-  /**
-   * @brief Get the age of the person.
-   * @return The age of the person as an integer.
-   */
-  int getAge() const;
-
-  /**
-   * @brief Get the person ID.
-   * @return The person ID as an integer.
-   */
-  int getPersonID() const;
-
-  /**
-   * @brief Set the name of the person.
-   * @param name The new name for the person.
-   */
-  void setName(const std::string& name);
-
-  /**
-   * @brief Set the age of the person.
-   * @param age The new age for the person.
-   */
-  void setAge(int age);
-
-  /**
-   * @brief Set the person ID.
-   * @param ID The new person ID.
-   */
-  void setPersonID(int ID);
-
-  /**
-   * @brief Print information about the person.
-   */
-  void printInfo() const;
-
-
-
 private:
-  std::string name; ///< The name of the person.
-  int age;          ///< The age of the person.
-  int ID;    ///< The uniqe person ID.
-
+ 
 }
 
 
-#endif // person_HPP
+#endif // PERSON_HPP
