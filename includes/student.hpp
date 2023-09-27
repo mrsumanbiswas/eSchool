c4df3a32fb48c81df490a410d9f9278b1b33119b
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-09-27 17:32:04 +0530

    student header file updated a bit

--- includes/student.hpp
+++ includes/student.hpp
@@ -27,7 +27,49 @@ public:
    */
   ~Student();
 
-  
+  /**
+   * @brief Get the name of the student.
+   * @return The name of the student as a string.
+   */
+  std::string getName() const;
+
+  /**
+   * @brief Get the age of the student.
+   * @return The age of the student as an integer.
+   */
+  int getAge() const;
+
+  /**
+   * @brief Get the student ID.
+   * @return The student ID as an integer.
+   */
+  int getStudentID() const;
+
+  /**
+   * @brief Set the name of the student.
+   * @param name The new name for the student.
+   */
+  void setName(const std::string& name);
+
+  /**
+   * @brief Set the age of the student.
+   * @param age The new age for the student.
+   */
+  void setAge(int age);
+
+  /**
+   * @brief Set the student ID.
+   * @param studentID The new student ID.
+   */
+  void setStudentID(int studentID);
+
+  /**
+   * @brief Print information about the student.
+   */
+  void printInfo() const;
+
+
+
 private:
   std::string name; ///< The name of the student.
   int age;          ///< The age of the student.
