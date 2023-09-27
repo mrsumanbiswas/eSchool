3834234963bb3ed3ccbc16fcc72b8818b5d0b71d
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-09-27 16:56:38 +0530

    student header file updated a bit

--- includes/student.hpp
+++ includes/student.hpp
@@ -1,4 +1,25 @@
+#ifdef STUDENT_HPP
+#define STUDENT_HPP
+
+#include <string>
+
 /**
-* @breif This is header file of Student Object 
-*  basicaly takes care of includes and all
+* @brief Represents a `student` object
 */
+
+class student{
+private:
+  std::string name; ///< The name of the student.
+  int age;          ///< The age of the student.
+  int studentID;    ///< The uniqe student ID.
+public:
+  /**
+  * @brief Default constructor for the Student class.
+  */
+  Student(); // Default constructor
+
+  
+}
+
+
+#endif // STUDENT_HPP
